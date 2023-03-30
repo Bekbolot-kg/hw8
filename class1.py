0631d5ef954fff985fad180bb957f6540834645b
Bekbolot-kg <aipidinovbekbolot.it04@gmail.com>
2023-03-30 19:05:06 +0600

    hw8

--- class1.py
+++ class1.py
@@ -1,3 +1,8 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+
+class Hi(Hello):
+
+    def __str__(self):
+        return f'Hello {self.str}'
--- task.py
+++ task.py
@@ -1,2 +1,8 @@
 from art import tprint
-tprint("PYTHON")
+# tprint("PYTHON")
+
+from class1 import Hi
+
+name = Hi('❤Bekbolot')
+
+tprint(str(name))
